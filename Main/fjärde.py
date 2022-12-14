20441bd7c7db2820106de2e5e8490690d66c9a1f
Skanda Nadiga <disturbedseal981@gmail.com>
2022-12-14 13:34:15 +0100

    changed unit to meters

--- Main/fjärde.py
+++ Main/fjärde.py
@@ -1,8 +1,8 @@
 import math
-peak1_height = float(input("Enter the height of the first peak: "))
-peak1_distance = float(input("Enter the distance of the first peak from you: "))
-peak2_height = float(input("Enter the height of the second peak: "))
-peak2_distance = float(input("Enter the distance of the second peak from you: "))
+peak1_height = float(input("Enter the height of the first peak in meters: "))
+peak1_distance = float(input("Enter the distance of the first peak from you in meters: "))
+peak2_height = float(input("Enter the height of the second peak in meters: "))
+peak2_distance = float(input("Enter the distance of the second peak from you meters: "))
 tan_angle = (peak1_height - peak2_height) / (peak1_distance - peak2_distance)
 angle = math.degrees(math.atan(tan_angle))
 if angle <= 0:
