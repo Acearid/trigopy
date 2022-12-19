15b65835ab577db490112c5e6b7e30f0323ed68f
Skanda Nadiga <disturbedseal981@gmail.com>
2022-12-19 16:03:45 +0100

    Update fjärde.py

--- Main/fjärde.py
+++ Main/fjärde.py
@@ -6,11 +6,11 @@ peak2_height = float(input("Enter the height of the second peak in meters: "))
 peak2_distance = float(input("Enter the distance of the second peak from you meters: "))
 # angle = arctan((h2-h1) / (d2-d1))
 tan_angle = (peak1_height - peak2_height) / (peak1_distance - peak2_distance)
+print(tan_angle)
 # calculate the angle
 angle = math.degrees(math.atan(tan_angle))
 #  If the angle is less than 0 degrees, then the second peak isn't visible; if it's greater than 0 degrees, then the second peak is visible.
 if angle <= 0:
         print("The second peak is not visible.")
 else:
-        print("The second peak is visible.")
-        
+        print("The second peak is visible.")       
