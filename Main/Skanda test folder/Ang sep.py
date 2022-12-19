4cdd7ebba1e0c1600dcff0b5538ca5cec0f24ba5
Skanda Nadiga <disturbedseal981@gmail.com>
2022-12-19 12:29:37 +0100

    Update Ang sep.py

--- Main/Skanda test folder/Ang sep.py
+++ Main/Skanda test folder/Ang sep.py
@@ -1,12 +1,4 @@
 import math
-def calculate_distance(dec1, dec2 , RA1, RA2):
-  delta_RA = RA2 - RA1
-    
- # Calculate the angular separation
-  angular_separation = math.sqrt((delta_RA * math.cos(dec2))**2 + (dec1 - dec2)**2)
-
-  print(angular_separation)
-
 
 # Get input from the user
 dec1 = float(input("Enter the declination of star 1, in degrees: "))
@@ -14,3 +6,15 @@ dev2 = float(input("Enter the declination of star 2, in degrees: "))
 RA1 = float(input("Enter the right ascension of star 1, in hours: "))
 RA2 = float(input("Enter the right ascension of star 2, in hours: "))
 
+
+def calculate_distance(dec1, dec2 , RA1, RA2):
+  delta_RA = RA2 - RA1
+    
+ # Calculate the angular separation
+  angular_separation = math.sqrt((delta_RA * math.cos(dec2))**2 + (dec1 - dec2)**2)
+
+  return(angular_separation)
+
+distance = calculate_distance(dec1, dev2, RA1, RA2)
+
+print(distance)
